82e4d82695f976a83d5d44da313ac940119c8472
kSaigusa <122540392+kSaigusa@users.noreply.github.com>
2023-03-11 15:05:22 +0900

    Create index-1.js

---
+++ index-1.js
@@ -0,0 +1,23 @@
+$(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date"]').datetimepicker({
+            step:15,
+        });
+    });
+
+    // 送信
+    $('form').submit(function () {
+        var date = $('input[name="date"]').val();
+        var name = $('input[name="name"]').val();
+        var number = $('input[name="phone-number"]').val();
+        var item = $("#select").children("option:selected");
+        var itemText = item.text();
+        var inumber = $('input[name="items-number"]').val();
+
+        var msg = `受け取り日時：${date}\n氏名：${name}\n電話番号：${number}\n注文商品：${itemText}\n個数：${inumber}`;
+        sendText(msg);
+
+        return false;
+    });
+});
