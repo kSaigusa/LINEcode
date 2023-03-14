f4ea78f7c7a0aa5958b27ecbdd61f18cfbe9f579
kSaigusa <122540392+kSaigusa@users.noreply.github.com>
2023-03-14 12:40:44 +0900

    Update and rename Gas.js to Gas-1.js

--- Gas-1.js
+++ Gas-1.js
@@ -24,16 +24,3 @@ function checkGmail() {
     properties.setProperty('lastNotifiedThreadId', threads[0].getId());
   }
 }
-
-function sendLineNotification(message) {
-  var token = 'アクセストークン'; // LINE Notifyのアクセストークンを入力してください
-  var options = {
-    'method': 'post',
-    'payload': 'message=' + message,
-    'headers': {
-      'Authorization': 'Bearer ' + token,
-    }
-  };
-  var response = UrlFetchApp.fetch('https://notify-api.line.me/api/notify', options);
-  Logger.log(response.getContentText());
-}
